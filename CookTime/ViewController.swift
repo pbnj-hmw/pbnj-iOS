f1eaf58dbe74d4cc9dd62c7d340b3b341ff3ef33
Reagan Wood <reaganwood1@gmail.com>
2018-07-21 18:08:34 -0500

    Pod file with snap kit, video fram now takes up half of the view.

--- CookTime/ViewController.swift
+++ CookTime/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController, BambuserViewDelegate {
 
@@ -27,7 +28,8 @@ class ViewController: UIViewController, BambuserViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        videoPlayer.applicationId = "C2i37bAE4rpiQbIEll9tXA"
-        bambuserView.orientation = UIApplication.shared.statusBarOrientation
+//        bambuserView.orientation = UIApplication.shared.statusBarOrientation
+        bambuserView.orientation = UIInterfaceOrientation.landscapeLeft
         self.view.addSubview(bambuserView.view)
 //        videoPlayer.playVideo("https://cdn.bambuser.net/broadcasts/ec968ec1-2fd9-f8f3-4f0a-d8e19dccd739?da_signature_method=HMAC-SHA256&da_id=432cebc3-4fde-5cbb-e82f-88b013140ebe&da_timestamp=1456740399&da_static=1&da_ttl=0&da_signature=8e0f9b98397c53e58f9d06d362e1de3cb6b69494e5d0e441307dfc9f854a2479")
         bambuserView.applicationId = "C2i37bAE4rpiQbIEll9tXA"
@@ -52,6 +54,9 @@ class ViewController: UIViewController, BambuserViewDelegate {
 //        viewMenuButton.addTarget(self, action: #selector(ViewController.viewMenu), for: UIControlEvents.touchUpInside)
 //        self.view.addSubview(viewMenuButton)
 //        // Do any additional setup after loading the view, typically from a nib.
+        
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
     }
 
     override func didReceiveMemoryWarning() {
@@ -71,10 +76,20 @@ class ViewController: UIViewController, BambuserViewDelegate {
     override func viewWillLayoutSubviews() {
         var statusBarOffset : CGFloat = 0.0
         statusBarOffset = CGFloat(self.topLayoutGuide.length)
-        bambuserView.previewFrame = CGRect(x: 0.0, y: 0.0 + statusBarOffset, width: self.view.bounds.size.width, height: self.view.bounds.size.height - statusBarOffset)
+//        bambuserView.previewFrame = CGRect(x: 0.0, y: 0.0 + statusBarOffset, width: self.view.bounds.size.width, height: self.view.bounds.size.height - statusBarOffset)
+        bambuserView.view.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.01)
+            make.height.equalToSuperview()
+        }
         
+//        broadcastButton.frame = CGRect(x: 0.0, y: 0.0 + statusBarOffset, width: 100.0, height: 50.0);
+        broadcastButton.snp.makeConstraints { (make) in
+            make.bottom.equalToSuperview().offset(-15)
+            make.right.equalToSuperview().multipliedBy(0.80)
+        }
         
-        broadcastButton.frame = CGRect(x: 0.0, y: 0.0 + statusBarOffset, width: 100.0, height: 50.0);
     }
     
     @objc func broadcast() {
@@ -127,4 +142,14 @@ class ViewController: UIViewController, BambuserViewDelegate {
 //        NSLog("Failed to load video for %@", videoPlayer.resourceUri);
 //    }
 //}
+
+extension ViewController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeLeft
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+}
 
