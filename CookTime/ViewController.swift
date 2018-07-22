614e4d462ba1f11938d0b0bc5c18472cb1676d19
Reagan Wood <reaganwood1@gmail.com>
2018-07-21 21:10:06 -0500

    More UI cleanup

--- CookTime/ViewController.swift
+++ CookTime/ViewController.swift
@@ -32,11 +32,7 @@ class ViewController: UIViewController, BambuserViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        videoPlayer.applicationId = "C2i37bAE4rpiQbIEll9tXA"
-//        bambuserView.orientation = UIApplication.shared.statusBarOrientation
         bambuserView.orientation = UIInterfaceOrientation.landscapeLeft
-        self.view.addSubview(bambuserView.view)
-//        videoPlayer.playVideo("https://cdn.bambuser.net/broadcasts/ec968ec1-2fd9-f8f3-4f0a-d8e19dccd739?da_signature_method=HMAC-SHA256&da_id=432cebc3-4fde-5cbb-e82f-88b013140ebe&da_timestamp=1456740399&da_static=1&da_ttl=0&da_signature=8e0f9b98397c53e58f9d06d362e1de3cb6b69494e5d0e441307dfc9f854a2479")
         bambuserView.applicationId = "C2i37bAE4rpiQbIEll9tXA"
         bambuserView.delegate = self;
         
@@ -48,21 +44,6 @@ class ViewController: UIViewController, BambuserViewDelegate {
         addAllElementsToView()
         setUIElements()
         bindUIElements()
-//        videoPlayer.delegate = self;
-//
-//        view.addSubview(videoPlayer)
-//
-//
-//        playButton.setTitle("Play", for: UIControlState.normal)
-//        playButton.addTarget(videoPlayer, action: #selector(BambuserPlayer.playVideo as (BambuserPlayer) -> () -> Void), for: UIControlEvents.touchUpInside)
-//        self.view.addSubview(playButton)
-//        pauseButton.setTitle("Pause", for: UIControlState.normal)
-//        pauseButton.addTarget(videoPlayer, action: #selector(BambuserPlayer.pauseVideo as (BambuserPlayer) -> () -> Void), for: UIControlEvents.touchUpInside)
-//        self.view.addSubview(pauseButton)
-//        viewMenuButton.setTitle("View Menu", for: UIControlState.normal)
-//        viewMenuButton.addTarget(self, action: #selector(ViewController.viewMenu), for: UIControlEvents.touchUpInside)
-//        self.view.addSubview(viewMenuButton)
-//        // Do any additional setup after loading the view, typically from a nib.
         
         let value = UIInterfaceOrientation.landscapeLeft.rawValue
         UIDevice.current.setValue(value, forKey: "orientation")
@@ -76,19 +57,10 @@ class ViewController: UIViewController, BambuserViewDelegate {
         view.addSubview(menuStepView)
     }
     
-//    override func viewWillLayoutSubviews() {
-//        let statusBarOffset = self.topLayoutGuide.length
-//        videoPlayer.frame = CGRect(x: 0, y: 0 + statusBarOffset, width: self.view.bounds.size.width, height: self.view.bounds.size.height - statusBarOffset)
-//
-//        playButton.frame = CGRect(x: 20, y: 20 + statusBarOffset, width: 100, height: 40)
-//        pauseButton.frame = CGRect(x: 20, y: 80 + statusBarOffset, width: 100, height: 40)
-//        viewMenuButton.frame = CGRect(x: 20, y: 140 + statusBarOffset, width: 100, height: 40)
-//    }
-    
     override func viewWillLayoutSubviews() {
         var statusBarOffset : CGFloat = 0.0
         statusBarOffset = CGFloat(self.topLayoutGuide.length)
-//        bambuserView.previewFrame = CGRect(x: 0.0, y: 0.0 + statusBarOffset, width: self.view.bounds.size.width, height: self.view.bounds.size.height - statusBarOffset)
+        
         bambuserView.view.snp.makeConstraints { (make) in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
@@ -96,7 +68,6 @@ class ViewController: UIViewController, BambuserViewDelegate {
             make.height.equalToSuperview()
         }
         
-//        broadcastButton.frame = CGRect(x: 0.0, y: 0.0 + statusBarOffset, width: 100.0, height: 50.0);
         broadcastButton.snp.makeConstraints { (make) in
             make.bottom.equalToSuperview().offset(-15)
             make.right.equalToSuperview().multipliedBy(0.80)
@@ -165,7 +136,6 @@ class ViewController: UIViewController, BambuserViewDelegate {
     }
     
     @objc func viewMenu() {
-//        videoPlayer.seek(to: 0.0);
         let mainStorybaord = UIStoryboard.init(name: "Main", bundle: nil)
         let menuVC = mainStorybaord.instantiateViewController(withIdentifier: "Menu")
         present(menuVC, animated: true, completion: nil)
@@ -185,27 +155,6 @@ class ViewController: UIViewController, BambuserViewDelegate {
         broadcastButton.addTarget(self, action: #selector(ViewController.broadcast), for: UIControlEvents.touchUpInside)
     }
 }
-
-//extension ViewController {
-//    func playbackStarted() {
-//        playButton.isEnabled = false
-//        pauseButton.isEnabled = true
-//    }
-//
-//    func playbackPaused() {
-//        playButton.isEnabled = true
-//        pauseButton.isEnabled = false
-//    }
-//
-//    func playbackStopped() {
-//        playButton.isEnabled = true
-//        pauseButton.isEnabled = false
-//    }
-//
-//    func videoLoadFail() {
-//        NSLog("Failed to load video for %@", videoPlayer.resourceUri);
-//    }
-//}
 
 extension ViewController {
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
