9c2303213cc18671024bb0cc99b0f5a1d058056f
Reagan Wood <reaganwood1@gmail.com>
2018-07-21 20:18:59 -0500

    MenuStepView now moves to the next element in the view and animates. It is awesome.

--- CookTime/MenuStepView.swift
+++ CookTime/MenuStepView.swift
@@ -12,7 +12,7 @@ import SnapKit
 
 class MenuStepView: UIView {
     private var instructions: [String] = ["0", "1", "2", "3", "4"]
-    private var currentIndex = 0
+    private var nextIndex = 1
     private var displayingLabel = UILabel()
     private var nextLabel = UILabel()
     
@@ -37,18 +37,55 @@ class MenuStepView: UIView {
     }
     
     public func animateNext() -> Bool {
-        guard currentIndex < instructions.count else { return false }
+        guard nextIndex < instructions.count else { return false }
         
-        displayingLabel.text = instructions[currentIndex]
-        currentIndex += 1
+        nextLabel.text = instructions[nextIndex]
+        nextIndex += 1
+        animateNextElement()
+        animateCurrentElementOffScreen()
         return true
+    }
+    
+    private func animateNextElement() {
+        layoutSubviews()
+        self.nextLabel.isHidden = false
+        UIView.animate(withDuration: 1, animations: { [weak self] in
+            self?.nextLabel.snp.remakeConstraints({ (make) in
+                make.centerX.equalToSuperview()
+                make.centerY.equalToSuperview()
+            })
+            self?.layoutIfNeeded()
+        })
+    }
+    
+    private func animateCurrentElementOffScreen() {
+        layoutSubviews()
+        UIView.animate(withDuration: 1.0, animations: { [weak self] in
+            self?.displayingLabel.snp.remakeConstraints({ [weak self] (make) in
+                guard let `self` = self else { return }
+                make.centerX.equalTo(self.snp.right)
+                make.centerY.equalToSuperview()
+            })
+            self?.layoutIfNeeded()
+        }) { [weak self] (_)
+            in
+            self?.displayingLabel.isHidden = true
+            var label = self?.nextLabel
+            self?.nextLabel = self?.displayingLabel ?? UILabel()
+            self?.displayingLabel = label ?? UILabel()
+            self?.nextLabel.snp.remakeConstraints({ (make) in
+                guard let `self` = self else { return }
+                make.centerX.equalTo(self.snp.left)
+                make.centerY.equalToSuperview()
+            })
+        }
     }
 
     public func animatePrevious() -> Bool {
-        guard currentIndex > 0 else { return false }
+        guard nextIndex > 0 else { return false }
         
-        currentIndex -= 1
-        displayingLabel.text = instructions[currentIndex]
+        nextIndex -= 1
+        displayingLabel.text = instructions[nextIndex]
         return true
     }
     
@@ -56,12 +93,18 @@ class MenuStepView: UIView {
         addSubview(displayingLabel)
         addSubview(nextLabel)
         nextLabel.isHidden = true
-        displayingLabel.text = instructions[currentIndex]
+        displayingLabel.text = instructions[0]
     }
     
     private func createConstraints() {
         displayingLabel.snp.makeConstraints { (make) in
             make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        nextLabel.snp.makeConstraints { [weak self] (make) in
+            guard let `self` = self else { return }
+            make.centerX.equalTo(self.snp.left)
             make.centerY.equalToSuperview()
         }
     }
